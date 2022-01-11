42c2b949bf35f6930e329bda56960263cbf6958c
jingyi-ju <96923360+jingyi-ju@users.noreply.github.com>
2022-01-11 14:21:58 -0500

    Create selectionSort.py

---
+++ selectionSort.py
@@ -0,0 +1,13 @@
+# **************************************************************************************************************
+
+def selection_sort(list):
+  ''' best and worst case runtime: Θ(n^2) '''
+    for i in range(0,len(list)-1):              # after n-1 iterations, n-1 smallest elements in L subarray
+        min = i
+        for j in range(i+1,len(list)):          # elements are compared in both B-case and W-case
+            if list[j] < list[min]:             
+                min = j
+        swap(list,min,i)
+
+
+# **************************************************************************************************************
