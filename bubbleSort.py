4bd270e58d7f71a58ad04a021314ea15afc074a3
jingyi-ju <96923360+jingyi-ju@users.noreply.github.com>
2022-01-11 14:43:47 -0500

    Update bubbleSort.py

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,17 +1,15 @@
-# **************************************************************************************************************
-
 def bubble_sort(list):
   ''' best and worst case: Θ(n^2) '''
+  
     for i in range(0, len(list)-1):                 # after n-1 iterations, n-1 largest elements in R subarray
         for j in range(0, len(list) - i -1):
             if list[j] > list[j + 1]:
                 swap(list, j, j + 1)
                
-# **************************************************************************************************************
-
 def optimized_bubble_sort(list):
   ''' best case: Θ(n) '''
   ''' worst case: Θ(n^2) '''
+  
     for i in range(0, len(list)-1):
         sorted = True
         for j in range(0, len(list) - i -1):        # best case: exit after i=0 iteration
@@ -20,5 +18,3 @@ def optimized_bubble_sort(list):
                 sorted = False
         if sorted:
             break
-            
-# **************************************************************************************************************
