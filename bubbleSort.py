444345d8a1986100829cc5e69d1603d864447627
jingyi-ju <96923360+jingyi-ju@users.noreply.github.com>
2022-01-11 14:13:33 -0500

    Create bubbleSort.py

---
+++ bubbleSort.py
@@ -0,0 +1,24 @@
+# *******************************************************
+
+def bubble_sort(list):
+  ''' best and worst case: Θ(n^2) '''
+    for i in range(0, len(list)-1):
+        for j in range(0, len(list) - i -1):
+            if list[j] > list[j + 1]:
+                swap(list, j, j + 1)
+               
+# *******************************************************
+
+def optimized_bubble_sort(list):
+  ''' best case: Θ(n) '''
+  ''' worst case: Θ(n^2) '''
+    for i in range(0, len(list)-1):
+        sorted = True
+        for j in range(0, len(list) - i -1):
+            if list[j] > list[j + 1]:
+                swap(list, j, j + 1)
+                sorted = False
+        if sorted:
+            break
+            
+# *******************************************************
